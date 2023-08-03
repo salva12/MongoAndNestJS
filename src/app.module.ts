0c5a2c212965928e3b4b9236a90b78cfc5121393
Salvador <@10250escalante>
2023-08-03 11:54:02 -0300

    perf: :fire: resolving merge conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import {MongoClient} from 'mongodb';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
 
+const uri = 'mongodb://root:root@localhost:27017/?authMechanism=DEFAULT'
+
+const client = new MongoClient(uri);
+
+async function run(){
+  await client.connect();
+  const database = client.db('platzi-store');
+  const taskCollection = database.collection('tasks');
+  const tasks = await taskCollection.find().toArray();
+  console.log('aca van las tasks',tasks);
+}
+
+run();
 @Module({
   imports: [],
   controllers: [AppController, ProductsController, CategoriesController],
